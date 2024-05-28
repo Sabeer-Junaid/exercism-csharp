f07c5589daca1be9c7aaa101d91f793ef6e45118
Sabeer Junaid <sabeerjunaid992@gmail.com>
2024-05-28 20:39:14 +0500

    Create Annalyn's Infiltration.cs

---
+++ Exercise/Annalyn's Infiltration.cs
@@ -0,0 +1,34 @@
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return prisonerIsAwake && !archerIsAwake;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent)
+        {
+            return !archerIsAwake;
+        } else
+        {
+            if (prisonerIsAwake)
+            {
+                return !knightIsAwake && !archerIsAwake;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
